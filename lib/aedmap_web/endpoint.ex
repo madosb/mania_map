cfe8deb360e4f2aec1de9fc37350dcd9d37d5629
Madhurya Singh <madhurya.singh@g.softbank.co.jp>
2019-02-17 16:56:31 +0900

    pre complete version

--- lib/aedmap_web/endpoint.ex
+++ lib/aedmap_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule AedmapWeb.Endpoint do
     at: "/",
     from: :aedmap,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt csv icon)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
